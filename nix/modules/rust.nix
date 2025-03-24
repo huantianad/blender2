e1d513ec7912f6b659741aafa4a2ba4fdfbf7a7d
huantian <davidtianli@gmail.com>
2025-03-23 23:54:15 -0700

    remove cargo-doc-live, nixfmt -> alejandra

--- nix/modules/rust.nix
+++ nix/modules/rust.nix
@@ -1,12 +1,15 @@
-{ inputs, ... }:
-{
+{inputs, ...}: {
   imports = [
     inputs.rust-flake.flakeModules.default
     inputs.rust-flake.flakeModules.nixpkgs
-    inputs.process-compose-flake.flakeModule
-    inputs.cargo-doc-live.flakeModule
   ];
-  perSystem = { config, self', pkgs, lib, ... }: {
+  perSystem = {
+    config,
+    self',
+    pkgs,
+    lib,
+    ...
+  }: {
     rust-project.crates."blender2".crane.args = {
       buildInputs = lib.optionals pkgs.stdenv.isDarwin (
         with pkgs.darwin.apple_sdk.frameworks; [
